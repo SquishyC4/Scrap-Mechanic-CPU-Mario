6f4a2488cf943d28412800ad0a645f40f16017dd
SquishyC4 <157242149+SquishyC4@users.noreply.github.com>
2024-01-20 20:30:36 +0000

    Update Assembler2re.py

--- Assembler2re.py
+++ Assembler2re.py
@@ -13,8 +13,8 @@ class Assembler:
         self.include = []
         self.machine_file = []
         self.flags = {'z':1,'nz':2,'c':4,'nc':8,'neg':16,'pos':32}
-        self.regs = {'r0','r1','r2','r3','r4','r5','r6','r7','r8','r9','r10','r11','r12','r13','r14','r15','r16','r17','r18','r19','r20','r21','r22','r23','r24','r25','r26','r27','r28','r29','r30','r31','p0','p1','p2','p3'}
         # zero, not zero, carry, no carry, negative, positive
+        self.regs = {'r0','r1','r2','r3','r4','r5','r6','r7','r8','r9','r10','r11','r12','r13','r14','r15','r16','r17','r18','r19','r20','r21','r22','r23','r24','r25','r26','r27','r28','r29','r30','r31','p0','p1','p2','p3'}
 
     def lex(self, path):
         tokens = Preprocessor.tokenise(path)
